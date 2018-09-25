e549a6f9c476dabcefd2c6855f3f8dc4f8ad7557
Glitch (voice-commands) <none>
2018-09-25 17:28:22 +0000

    🎱⛹ Checkpoint ./script.js:329397/1057 ./index.html:329397/96 ./style.css:329397/175

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 try {
   var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   var recognition = new SpeechRecognition();
-} catch (ex) {
+  var instructions = document.getElementById("instructions");
+  var capturedText = "";
+} catch (ex) { // It is fairly experimental technology, if no support, hide the app.
   console.error(ex);
   alert("No Browser support for WebAPI");
-  document.getElementById("no-browser").classList.remove()
+  document.getElementById("no-browser").classList.remove("hidden");
+  document.getElementById("app").classList.add("hidden");
+}
+
+recognition.onstart = () => {
+  instructions.innerHTML = "Listening for voice input"
+}
+
+recognition.onspeechend = () => {
+  instructions.innerHTML = "<span class='important'>Speech detection stopped.</span>"
+}
+
+recognition.onerror = (e) => {
+  console.error(e);
+  (e.error == "no-speech") ? instructions.innerHTML = "No speech detected." : instructions.innerHTML = "Error occured";
+}
+
+recognition.onresult = (e) => {
+  let current = e.resultIndex;
+  
+  let transcript = e.results[current][0].transcript;
+  
+  capturedText += transcript;
+  document.getElementById("text-area").value = capturedText;
 }
